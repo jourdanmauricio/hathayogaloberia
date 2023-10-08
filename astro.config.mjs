8928ab2f833bbb23cb5fb75bc04fb47ccd90ac64
jourdanmauricio <jourdanmauricio@gmail.com>
2023-10-07 22:09:34 -0300

    Add og protocol

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    astroOGImage({
+      config: {
+        path: '/hathayogaloberia/posts', // change this value to the folder where your posts are
+        // NOTE: index.md file will not get proccesed, so please avoid it
+      },
+    }),
+  ],
   site: 'https://jourdanmauricio.github.io',
   base: '/hathayogaloberia',
 });
